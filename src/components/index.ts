1b890214998ce43b4e93ae77be0b0033ede2ca28
almosten97 <almosten97@gmail.com>
2026-01-15 17:38:47 +0000

    add vitest workflow and update test scripts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,4 @@
 export { default as AdminDashboard } from './AdminDashboard';
 export { default as EmployeeDashboard } from './EmployeeDashboard';
-export { default as LoginPage } from './LoginPage';
+export { default as Login } from './Login';
 export { default as NoRole } from './NoRole';
